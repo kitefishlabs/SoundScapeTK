fec90021c17ee97ff72be030ea5f9a1a4c11fce7
tms <tms@kitefishlabs.com>
2014-12-03 15:02:47 -0600

    Added pod files.

---
+++ SoundScapeTK-iOS/Pods/Pods-WheelWithinWheel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WheelWithinWheel : NSObject
+@end
+@implementation PodsDummy_Pods_WheelWithinWheel
+@end
---
+++ SoundScapeTK-iOS/Pods/Pods-WheelWithinWheel-libpd-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WheelWithinWheel_libpd : NSObject
+@end
+@implementation PodsDummy_Pods_WheelWithinWheel_libpd
+@end
